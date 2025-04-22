6b1c182a5aebdcfd5a63a1a8d5a93751aad69547
Nia <shalise.nia@gmail.com>
2025-04-22 09:34:47 -0400

    Added features folder and shared folders; moved components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import './App.css';
-import TodoList from './TodoList';
-import TodoForm from './TodoForm';
+import TodoList from './features/TodoList/TodoList';
+import TodoForm from '../src/features/TodoForm';
 import { useState } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
 
-
   const handleAdd = (workingTodo) => {
-    const newTodo = {key: Date.now(), workingTodo, isCompleted: false}
+    const newTodo = { key: Date.now(), workingTodo, isCompleted: false };
     setTodoList([...todoList, newTodo]);
-  }
+  };
 
   function completeTodo(key) {
     const updatedTodo = todoList.map((todo) => {
       if (todo.key === key) {
-        return {...todo, isCompleted: true};
+        return { ...todo, isCompleted: true };
       }
-        return todo;
+      return todo;
     });
 
     setTodoList(updatedTodo);
@@ -27,7 +26,7 @@ function App() {
     <div>
       <h1>My Todos</h1>
       <TodoForm handleAdd={handleAdd} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
---
+++ src/features/TodoList/TodoList.jsx
@@ -0,0 +1,26 @@
+{
+  /*extract from TodoList.jsx */
+}
+import TodoListItem from '../TodoList/TodoListItem';
+
+function TodoList({ todoList, onCompleteTodo }) {
+  const filteredTodoList = todoList.filter(
+    (todo) => todo.isCompleted === false
+  );
+
+  return todoList.length === 0 ? (
+    <p>Add a todo above to get started</p>
+  ) : (
+    <ul>
+      {filteredTodoList.map((todo) => (
+        <TodoListItem
+          key={todo.key}
+          todo={todo}
+          onCompleteTodo={onCompleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
