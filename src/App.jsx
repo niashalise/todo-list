7510ceaecee55d89f695cfd2a3e6a0cabc2347df
Nia <shalise.nia@gmail.com>
2025-04-29 21:43:30 -0400

    Instructions part 1-3, still working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from '../src/features/TodoForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isShowing, setIsShowing] = useState(true);
+  const [isSaving, setIsSaving] = useState(false);
 
-  const handleAdd = (workingTodo) => {
-    const newTodo = { key: Date.now(), workingTodo, isCompleted: false };
-    setTodoList([...todoList, newTodo]);
+  const handleAdd = async (workingTodo) => {
+    const payload = {
+      records: [{
+        fields: {
+          title: workingTodo.title,
+          isCompleted: workingTodo.isCompleted
+        }
+      }]
+    }
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    }
+
+    try {
+      setIsSaving(true);
+      const resp = await fetch(url, options)
+      if (!resp.ok) {
+        throw new Error(resp.message)
+      }
+      const { records } = await resp.json();
+      const savedTodo = {
+        id: records[0].id,
+        title: records.fields.title,
+        isCompleted: records.fields.isCompleted
+      }
+      if (!records[0].fields.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+      setTodoList([...todoList, savedTodo]);
+    }
+    catch {
+      console.log(errorMessage);
+      setErrorMessage(errorMessage)
+    }
+    finally {
+      setIsSaving(false);
+    }
   };
 
   function completeTodo(key) {
@@ -34,11 +77,65 @@ function App() {
     setTodoList(updatedTodos)
   }
 
+const updateLoading = () => {
+  setIsLoading(prevState => !prevState);
+}
+
+const updateErrorMessage = () => {
+  setErrorMessage();
+}
+
+const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+useEffect(
+    () => {
+    const fetchTodos = async () => {
+    setIsLoading(true);
+    const options = {
+      method: "GET",
+      body: JSON.stringify(),
+      headers: {Authorization: token}
+    }
+  try {
+    const resp = await fetch(url, options);
+    console.log(resp);
+    if (resp.ok === false) {
+      throw new Error(resp.message)
+    }
+    const data = await resp.json();
+    console.log(data);
+    const todoRecords = data.records.map((record) => {
+      const todo = {
+        id: record.id,
+        createdTime: record.createdTime,
+        title: record.fields.title
+      };
+    })
+    console.log(todoRecords);
+  }
+  catch (error) {
+    setErrorMessage(error.message)
+  }
+  finally {
+    setIsLoading(false);
+  }
+  }
+  fetchTodos();
+  }, [])
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm handleAdd={handleAdd} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+        isLoading={isLoading ? <p>Add a todo above to get started</p> : <p>Todo list loading...</p>}
+      />
+      {errorMessage ? <div><hr /><p>{errorMessage}</p><button type="button" onClick={() => {setIsShowing(prevState => !prevState)}}>Dismiss</button> </div> : <></>}
     </div>
   );
 }
